840b7cafa060f300ee6dc6ec85c662e5f4737ceb
doodleman218 <dcwolff218@gmail.com>
2020-09-18 15:58:13 -0500

    card.js creation and code

---
+++ jason_carousel/src/components/card.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Card = props => {
+  const {imgSrc, classList} = props
+
+  return (
+    <div className={classList}>
+      <img src={imgSrc}
+      className='rootbeer-img'
+      alt='root beer img' />
+    </div>
+  )
+}
+
+
+
+export default Card;
