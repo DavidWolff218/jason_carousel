306cfa65bf105fba47666823abb3df6b067c47c4
doodleman218 <dcwolff218@gmail.com>
2020-09-18 16:02:26 -0500

    cardContainer start

---
+++ jason_carousel/src/components/cardContainer.js
@@ -0,0 +1,11 @@
+import {
+  rootBeer1,
+  rootBeer2,
+  rootBeer3,
+  rootBeer4
+} from '../images/index'
+
+<Card imgSrc={rootBeer1} classList={setClass(1)}/>
+<Card imgSrc={rootBeer2} classList={setClass(2)}/>
+<Card imgSrc={rootBeer3} classList={setClass(3)}/>
+<Card imgSrc={rootBeer4} classList={setClass(4)}/>
