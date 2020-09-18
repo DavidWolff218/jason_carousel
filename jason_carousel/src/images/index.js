1ba3b6fb77df7c1e43cdc920f141265bfe9a1685
doodleman218 <dcwolff218@gmail.com>
2020-09-18 14:10:53 -0500

    folder creation and import/export of images

---
+++ jason_carousel/src/images/index.js
@@ -0,0 +1,4 @@
+export const rootBeer1 = require(`./root-beer-1.jpg`)
+export const rootBeer2 = require(`./root-beer-2.jpg`)
+export const rootBeer3 = require(`./root-beer-3.jpg`)
+export const rootBeer4 = require(`./root-beer-4.webp`)
